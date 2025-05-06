ab498cbb826e22ad2ca2fb9cfe78f307e05f986a
Ryoku2217 <jorge.ramirez9243@alumnos.udg.mx>
2025-05-06 00:13:09 -0600

    CAMBIOS

--- isaacScript.js
+++ isaacScript.js
@@ -16,6 +16,26 @@ const iconosStats = {
   bomba: "icons/bomb.webp"
 };
 
+// Definición de iconos logros
+
+const iconosLogros = {
+  "Isaac's Head": "icons/Isaac'sHead.webp",
+  "Lost Baby": "icons/lost-baby.webp",
+  "Isaac's Tears": "icons/isaac's-tears.webp",
+  "Mom's Knife": "icons/mom's-knife.webp",
+  "Missing Poster": "icons/missing-poster.webp",
+  "The D20": "icons/the-d20.webp",
+  "Lil' Chest": "icons/lil'-chest.webp",
+  "D1": "icons/d1.webp",
+  "Fart Baby": "icons/fart-baby.webp",
+  "Cry Baby": "icons/cry-baby.webp",
+  "D infinity": "icons/d-infinity.webp",
+  "Meat Cleaver": "icons/meatcleaver.webp",
+  "Options?": "icons/options.webp",
+  "The Broken": "icons/the-broken.webp",
+  "Buddy Baby": "icons/buddy-baby.webp"
+};
+
 // Definición de Isaac normal
 
 const isaacNormal = {
@@ -33,23 +53,23 @@ const isaacNormal = {
   ],
   items: "The D6 (desbloqueable)",
   descripcion: "Isaac es el personaje principal de The Binding of Isaac. Es el único personaje desbloqueado por defecto. Es un niño que vive con su madre, quien lo encierra en su habitación y le prohíbe salir. Un día, su madre escucha una voz que le dice que debe sacrificar a Isaac para demostrar su fe. Isaac escapa al sótano y se enfrenta a monstruos y jefes en su camino. Isaac comienza con 3 corazónes rojos  y una bomba . También comenzara con el D6 una vez lo hayas desbloqueado.",
-  logros: [
-    "Isaac's Head - Completa Boss Rush",
-    "Lost Baby - Derrota a Mom's Heart en modo difícil",
-    "Isaac's Tears - Derrota a Isaac",
-    "Mom's Knife - Derrota a Satan",
-    "Missing Poster - Derrota a The Lamb",
-    "The D20 - Derrota a ???",
-    "Lil' Chest - Derrota a Ultra Greed",
-    "D1 - Derrota a Ultra Greedier",
-    "Fart Baby - Derrota a Hush",
-    "Cry Baby - Derrota a Mega Satan",
-    "D infinity - Derrota a Delirium",
-    "Meat Cleaver - Derrota a Mother",
-    "Options? - Derrota a The Beast",
-    "The Broken - Usa la Red Key para abrir el cuarto oculto en Casa",
-    "Buddy Baby - Completa todas las marcas en modo dificil"
-  ],
+  logros: {
+    "Isaac's Head": "Completa Boss Rush",
+    "Lost Baby": "Derrota a Mom's Heart en modo difícil",
+    "Isaac's Tears": "Derrota a Isaac",
+    "Mom's Knife": "Derrota a Satan",
+    "Missing Poster": "Derrota a The Lamb",
+    "The D20": "Derrota a ???",
+    "Lil' Chest": "Derrota a Ultra Greed",
+    "D1": "Derrota a Ultra Greedier",
+    "Fart Baby": "Derrota a Hush",
+    "Cry Baby": "Derrota a Mega Satan",
+    "D infinity": "Derrota a Delirium",
+    "Meat Cleaver": "Derrota a Mother",
+    "Options?": "Derrota a The Beast",
+    "The Broken": "Usa la Red Key para abrir el cuarto oculto en Casa",
+    "Buddy Baby": "Completa todas las marcas en modo dificil"
+  },
   trivia: [
     "Isaac hace referencia a la historia bíblica de la unión de Isaac, en la que Dios le ordenó a Abraham sacrificar a su único hijo, Isaac, para demostrar su lealtad y amor. \"Entonces Dios le dijo: 'Toma a tu hijo, tu único hijo, a quien amas, Isaac, y ve a la región de Moriah. Sacrifícalo allí como holocausto en el monte que yo te mostraré'\" (Génesis 22:2).",
     "Edmund ha declarado que Isaac es zurdo, pero en la escena inicial, Isaac sostiene un lápiz con la mano derecha. Esto podría ser una referencia al prejuicio católico contra la zurdera, derivado de Mateo 25:33, que proclama que Dios pondrá a los que merecen ascender al Cielo a su derecha y a los que merecen descender al Infierno a su izquierda. La madre de Isaac podría haberlo obligado a trabajar con la mano derecha. La práctica general de obligar a los zurdos a usar la mano derecha también ha sido una práctica desde hace mucho tiempo, y la aceptación de los zurdos solo comenzó a notarse en 1971.",
@@ -80,15 +100,15 @@ const taintedIsaac = {
     "Tainted Isaac solo puede llevar 8 objetos pasivos a la vez. Sus 8 objetos pasivos actuales se ven en la esquina superior izquierda de la pantalla, y uno de ellos está resaltado con un cuadrado blanco. Al recoger un noveno objeto pasivo, el objeto seleccionado, o mejor dicho, el que se encuentra en el cuadrado blanco, se colocará en un pedestal frente a Isaac Corrupto. Puedes pulsar la tecla de intercambio para alternar entre los objetos seleccionados.",
     "Ten cuidado al usar a Tainted Isaac mientras desbloqueas The Forgotten, ya que la segunda pieza de la pala se puede volver a enrollar."
   ],
-  logros: [
-    "Mom's Lock - Derrota a Isaac, ???, Satan y The Lamb con Tainted Isaac.",
-    "The Stars? - Derrota a Ultra Greedier con Tainted Isaac.",
-    "Soul of Isaac - Derrota a Hush y a la Boss Rush con Tainted Isaac.",
-    "Mega Chest - Derrota a Mega Satan con Tainted Isaac.",
-    "Spindow Dice - Derrota a Delirium con Tainted Isaac.",
-    "Dice Bag - Derrota a Mother con Tainted Isaac.",
-    "Glitched Crown - Derrota a The Beast con Tainted Isaac."
-  ],
+  logros: {
+    "Mom's Lock": "Derrota a Isaac, ???, Satan y The Lamb con Tainted Isaac.",
+    "The Stars?": "Derrota a Ultra Greedier con Tainted Isaac.",
+    "Soul of Isaac": "Derrota a Hush y a la Boss Rush con Tainted Isaac.",
+    "Mega Chest": "Derrota a Mega Satan con Tainted Isaac.",
+    "Spindow Dice": "Derrota a Delirium con Tainted Isaac.",
+    "Dice Bag": "Derrota a Mother con Tainted Isaac.",
+    "Glitched Crown": "Derrota a The Beast con Tainted Isaac."
+  },
   trivia: [
    "El logro por desbloquear a Tainted Isaac es `El Roto`. Este es el nombre que se usa en The Binding of Isaac: Four Souls.",
    "El desbloqueo de Tainted Isaac para Ultra Greedier es la XVII - The Stars?. Normalmente, esta carta simboliza la falta de fe y la desesperación. Probablemente se refiere a que Isaac se considera malvado y cree que su única salida es suicidarse."
@@ -162,16 +182,23 @@ function alternarTainted() {
     // Actualizar logros
 
     const logrosHTML = Object.entries(personaje.logros).map(([key, value]) => {
-      const icon = iconosStats[key] || "";
+      const icon = iconosLogros[key] || "";
+      if (icon) {
+        console.log(`Icono para ${key}: ${icon}`);
+      } else {
+        console.log(`No hay icono para ${key}`);
+        console.log(iconosLogros["Isaac's Head"]); // Debería mostrar el camino del icono correspondiente
+
+      }
       return `
-        <p>
-          ${icon ? `<img src="${icon}" alt="${key}" class="stat-icon">` : ""}
-          <strong>${capitalizar(key)}:</strong> ${value}
-        </p>
+        <li>
+          ${icon ? `<img src="${icon}" alt="${key}" class="logros-icons">` : ""}
+          <strong>${capitalizar(key)} - ${value}</strong>
+        </li>
       `;
     }).join("");
     
-    document.querySelector(".stats").innerHTML = statsHTML;
+    document.getElementById("logros-contenido").innerHTML = logrosHTML;
 
     // Actualizar trivia
 
